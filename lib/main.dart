092dc805821d4a647f026c97bbd2629543e00f39
laoboli <1293528695@qq.com>
2025-04-02 10:41:07 +0800

    refactor: font.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // main.dart
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -22,10 +24,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Path Manager',
+      title: 'Quick Dir',
       navigatorObservers: [FlutterSmartDialog.observer],
       builder: FlutterSmartDialog.init(),
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blue,fontFamily: Platform.isWindows ? 'HarmonyOS' : null),
       home: const MainScreen(),
     );
   }
